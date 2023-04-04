4456ac466c6f931b2ccc022a05b0f65cd4649aae
nikusrotuslomatus <jiornojovanagoldexp@gmail.com>
2023-04-04 06:42:01 +0300

    Bipolarka ] model interactions on the frontend gAdd project files.

---
+++ Models/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Social_Media.Models
+{
+    public class AppUser: IdentityUser
+    {
+        public int? Chords { get; set; }
+        public int? Songs { get; set; }
+        public Address? Address { get; set; }
+        public ICollection<Concert> Concerts { get; set; }
+        public ICollection<Group> Groups { get; set; }
+
+
+
+
+    }
+}
